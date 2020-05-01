8d47fb5b738dcce6327f49b5be49c2e0d8cd9ac7
jgarciagonj97 <j.garciagonj@gmail.com>
2020-05-01 14:51:21 +0200

    Funcionalidades de: añadir, controlar campos vacíos y color de fondo de etiqueta hechas.

--- js/funciones.js
+++ js/funciones.js
@@ -1,24 +1,72 @@
 var boton = $('#btn');
-var tareas = $('.deberes');
-var borrar = $('.borrar');
+var tareas = document.querySelector('.deberes');
+var mensaje = document.querySelector('#mensaje');
+var contador = 0;
 
-boton.on('click', asignaTarea);
-borrar.on('click', borrarTarea);
-
-function asignaTarea(event) {
+boton.on('click', function (event) {
     event.preventDefault();
+    mensaje.innerText = "";
 
-    tareas.html(`<div class="apartado">
-                    <div>
-                        <h3>Estudiar Javascript</h3>
-                    </div>
-                    <div class="borrar">
-                        <h3>Eliminar</h3>
-                    </div>
-                </div>`);
+    let nombre = document.querySelector('#nombreTarea').value;
+    let prioridad = document.querySelector('#prioridad').value;
+
+    if (nombre.length == 0 || prioridad.length == "") {
+        mensaje.innerText = 'Debes introducir los dos campos';
+    } else {
+        guardarDatos(nombre, prioridad);
+    }
+
+    document.querySelector('#nombreTarea').value = "";
+    document.querySelector('#prioridad').value = "";
+    console.log(listaTareas);
+});
+
+
+
+function guardarDatos(pNombre, pPrioridad) {
+
+    let registro = {
+        idTarea: contador,
+        titulo: pNombre,
+        prioridad: pPrioridad,
+    }
+
+    contador++;
+    listaTareas.push(registro);
+    pintarTarea(registro);
 }
 
-function borrarTarea() {
-    console.log('hola');
-    tareas.html();
+
+function pintarTarea(pObjeto) {
+    let prioridad = document.querySelector('#prioridad').value;
+    var color = "";
+
+    switch (prioridad) {
+        case "Urgente":
+            color = "tomato";
+            break;
+        case "Diaria":
+            color = "rgb(252, 243, 78)";
+            break;
+        case "Mensual":
+            color = "rgb(0, 255, 0)";
+            break;
+    }
+    tareas.innerHTML += (`<div class="apartado">
+                        <div style="background-color: ${color}">
+                            <h3>${pObjeto.titulo}</h3>
+                        </div>
+                        <div class="borrar">
+                            <h3>Eliminar</h3>
+                        </div>
+                    </div>`);
+
+    // var borrar = $('.borrar');
+    // borrar.on('click', borrarTarea);
 }
+
+
+// function borrarTarea() {
+//     console.log('hola');
+//     tareas.remove(this);
+// }
--- js/modelo.js
+++ js/modelo.js
@@ -1,19 +1,1 @@
 var listaTareas = new Array();
-
-listaTareas = [
-    {
-        idTarea: 0,
-        titulo: 'Estudiar Javascript',
-        prioridad: 'urgente',
-    },
-    {
-        idTarea: 1,
-        titulo: 'Estudiar Javascript',
-        prioridad: 'urgente',
-    },
-    {
-        idTarea: 2,
-        titulo: 'Estudiar Javascript',
-        prioridad: 'urgente',
-    },
-]
